f45f4b600c8a2d97630bc31793a8b476d67162bf
Ronald <kyoberonald@gmail.com>
2020-04-28 22:26:07 +0000

    disabled the deleting of a category with associated posts. Disabled the deleting of tags associated with posts.

--- cms/app/Http/Controllers/CategoriesController.php
+++ cms/app/Http/Controllers/CategoriesController.php
@@ -105,6 +105,13 @@ class CategoriesController extends Controller
      */
     public function destroy(Category $category)
     {
+        if ($category->posts->count()>0) {
+
+            session()->flash('error', 'Category cannot be deleted because it has some posts');
+  
+            return redirect()->back();
+        }
+
         $category->delete();
 
         session()->flash('success', 'Category deleted successfully');
--- cms/app/Http/Controllers/TagsController.php
+++ cms/app/Http/Controllers/TagsController.php
@@ -105,6 +105,13 @@ class TagsController extends Controller
      */
     public function destroy(Tag $tag)
     {
+        if ($tag->posts->count()>0) {
+           
+            session()->flash('error', 'Tag cannot be deleted because it is associated with some posts');
+
+            return redirect()->back();
+        }
+
         $tag->delete();
 
         session()->flash('success', 'Tag deleted successfully');
